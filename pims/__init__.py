451d1934407f3b28bf2d9c46d06119d7c1336747
Ulysse Rubens <urubens@uliege.be>
2020-10-27 10:23:05 +0100

    Make the project installable

--- pims/__init__.py
+++ pims/__init__.py
@@ -2,3 +2,4 @@
 from .__version__ import __title__, __description__, __url__
 from .__version__ import __version__
 from .__version__ import __license__, __copyright__
+from .__version__ import __author__, __email__
