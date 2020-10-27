78305b2d4dda430883bf6e065731518a9c09c7ec
Ulysse Rubens <urubens@uliege.be>
2020-10-27 10:40:54 +0100

    Improve versioning

--- pims/__version__.py
+++ pims/__version__.py
@@ -1,7 +1,10 @@
+
+VERSION = (0, 1, 0)
+
 __title__ = 'pims'
 __description__ = 'Cytomine Python Image Server'
 __url__ = 'https://doc.uliege.cytomine.org'
-__version__ = '0.1.0'
+__version__ = '.'.join(map(str, VERSION))
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2020 Cytomine.org'
 __author__ = 'cytomine.org'
