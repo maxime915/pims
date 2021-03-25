efa97d965839aa0d31a1cf62ac48ba94a0223d81
Ulysse Rubens <urubens@uliege.be>
2021-03-25 10:40:48 +0100

    Fix window processing in 16 bits

--- pims/processing/operations.py
+++ pims/processing/operations.py
@@ -142,6 +142,12 @@ class ResizeImgOp(ImageOp):
 
     def _vips_impl(self, img, *args, **kwargs):
         if img.width != self.width or img.height != self.height:
+
+            if img.interpretation in ("grey16", "rgb16"):
+                # Related to https://github.com/libvips/libvips/issues/1941 ?
+                return img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE, linear=True) \
+                    .colourspace(img.interpretation)
+            
             img = img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE)
         return img
 
