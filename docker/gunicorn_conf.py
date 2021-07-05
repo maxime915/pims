cb79c4197a1d30f58680d2429e6b266d1cf5ddb7
Ulysse Rubens <urubens@uliege.be>
2021-07-05 13:58:21 +0200

    Improve logging for Docker and prod

---
+++ docker/gunicorn_conf.py
@@ -0,0 +1,95 @@
+import json
+import logging.config
+import multiprocessing
+import os
+
+import yaml
+
+
+def configure_logging(default_log_level='info'):
+    log_config = os.getenv("LOG_CONFIG_FILE")
+    default_config = not bool(log_config)
+    if default_config:
+        log_config = "/app/logging-prod.yml"
+
+    if log_config.endswith(".json"):
+        with open(log_config) as file:
+            loaded_config = json.load(file)
+    elif log_config.endswith((".yaml", ".yml")):
+        with open(log_config) as file:
+            loaded_config = yaml.safe_load(file)
+    else:
+        print("Log config ignored: unsupported format")
+        return
+
+    if default_config:
+        loaded_config['root']['level'] = default_log_level.upper()
+    logging.config.dictConfig(loaded_config)
+    return loaded_config
+
+
+workers_per_core_str = os.getenv("WORKERS_PER_CORE", "1")
+max_workers_str = os.getenv("MAX_WORKERS")
+use_max_workers = None
+if max_workers_str:
+    use_max_workers = int(max_workers_str)
+web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
+
+host = os.getenv("HOST", "0.0.0.0")
+port = os.getenv("PORT", "80")
+bind_env = os.getenv("BIND", None)
+use_loglevel = os.getenv("LOG_LEVEL", "info")
+if bind_env:
+    use_bind = bind_env
+else:
+    use_bind = f"{host}:{port}"
+
+cores = multiprocessing.cpu_count()
+workers_per_core = float(workers_per_core_str)
+default_web_concurrency = workers_per_core * cores
+if web_concurrency_str:
+    web_concurrency = int(web_concurrency_str)
+    assert web_concurrency > 0
+else:
+    web_concurrency = max(int(default_web_concurrency), 2)
+    if use_max_workers:
+        web_concurrency = min(web_concurrency, use_max_workers)
+accesslog_var = os.getenv("ACCESS_LOG", "-")
+use_accesslog = accesslog_var or None
+errorlog_var = os.getenv("ERROR_LOG", "-")
+use_errorlog = errorlog_var or None
+graceful_timeout_str = os.getenv("GRACEFUL_TIMEOUT", "120")
+timeout_str = os.getenv("TIMEOUT", "120")
+keepalive_str = os.getenv("KEEP_ALIVE", "5")
+
+# Gunicorn config variables
+logconfig_dict = configure_logging(use_loglevel)
+loglevel = use_loglevel
+workers = web_concurrency
+bind = use_bind
+errorlog = use_errorlog
+worker_tmp_dir = "/dev/shm"
+accesslog = use_accesslog
+graceful_timeout = int(graceful_timeout_str)
+timeout = int(timeout_str)
+keepalive = int(keepalive_str)
+
+
+# For debugging and testing
+if True or loglevel.upper() == "DEBUG":
+    log_data = {
+        "loglevel": loglevel,
+        "workers": workers,
+        "bind": bind,
+        "graceful_timeout": graceful_timeout,
+        "timeout": timeout,
+        "keepalive": keepalive,
+        "errorlog": errorlog,
+        "accesslog": accesslog,
+        # Additional, non-gunicorn variables
+        "workers_per_core": workers_per_core,
+        "use_max_workers": use_max_workers,
+        "host": host,
+        "port": port,
+    }
+    print(json.dumps(log_data))
--- pims/logger.py
+++ pims/logger.py
@@ -1,8 +1,12 @@
+import logging
+import re
 from logging import LogRecord
 
 from rich.console import ConsoleRenderable
 from rich.logging import RichHandler
 from rich.text import Text
+
+RICH_FORMAT_REGEX = re.compile("[\[].*?[\]]")
 
 
 class PimsHandler(RichHandler):
@@ -27,3 +31,25 @@ class PimsHandler(RichHandler):
         if self.KEYWORDS:
             message_text.highlight_words(self.KEYWORDS, "logging.keyword")
         return message_text
+
+
+class StdoutFormatter(logging.Formatter):
+    def format(self, record: LogRecord) -> str:
+        record.message = re.sub(RICH_FORMAT_REGEX, "", record.getMessage())
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        s = self.formatMessage(record)
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + record.exc_text
+        if record.stack_info:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + self.formatStack(record.stack_info)
+        return s
