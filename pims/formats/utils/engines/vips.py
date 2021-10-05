maxime915/pims

commit 336b2681f8d6ff353053139746278f91bc1ebde4

Fix resize with 16bit data with libvips

pims/formats/utils/engines/vips.py
@@ -91,14 +91,15 @@ class VipsReader(AbstractReader):
         return filename
 
     def vips_thumbnail(self, width, height, **loader_options):
-        image = cached_vips_file(self.format)
-
         filename = self.vips_filename_with_options(str(self.format.path), **loader_options)
-        if image.interpretation in ("grey16", "rgb16"):
-            # Related to https://github.com/libvips/libvips/issues/1941 ?
-            return VIPSImage.thumbnail(filename, width, height=height,
-                                       size=VIPSSize.FORCE, linear=True) \
-                .colourspace(image.interpretation)
+
+        # Seems it has been fixed by https://github.com/libvips/libvips/pull/2120
+        # image = cached_vips_file(self.format)
+        # if image.interpretation in ("grey16", "rgb16"):
+        #     # Related to https://github.com/libvips/libvips/issues/1941 ?
+        #     return VIPSImage.thumbnail(filename, width, height=height,
+        #                                size=VIPSSize.FORCE, linear=True) \
+        #         .colourspace(image.interpretation)
 
         return VIPSImage.thumbnail(filename, width, height=height, size=VIPSSize.FORCE)
 
@@ -126,12 +127,12 @@ class VipsHistogramReader(NullHistogramReader):
             return cached_vips_file(self.format)
 
         def _thumb(format):
-            image = cached_vips_file(format)
-            if image.interpretation in ("grey16", "rgb16"):
-                return VIPSImage.thumbnail(str(format.path), 1024, linear=True)\
-                    .colourspace(image.interpretation)
-            else:
-                return VIPSImage.thumbnail(str(format.path), 1024)
+            # Seems it has been fixed by https://github.com/libvips/libvips/pull/2120
+            # image = cached_vips_file(format)
+            # if image.interpretation in ("grey16", "rgb16"):
+            #     return VIPSImage.thumbnail(str(format.path), 1024, linear=True)\
+            #         .colourspace(image.interpretation)
+            return VIPSImage.thumbnail(str(format.path), 1024)
         return self.format.get_cached('_vips_hist_image', _thumb, self.format)
 
     def type(self):

pims/processing/operations.py
@@ -126,11 +126,11 @@ class ResizeImgOp(ImageOp):
 
     def _vips_impl(self, img, *args, **kwargs):
         if img.width != self.width or img.height != self.height:
-
-            if img.interpretation in ("grey16", "rgb16"):
-                # Related to https://github.com/libvips/libvips/issues/1941 ?
-                return img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE, linear=True) \
-                    .colourspace(img.interpretation)
+            # Seems it has been fixed by https://github.com/libvips/libvips/pull/2120
+            # if img.interpretation in ("grey16", "rgb16"):
+            #     # Related to https://github.com/libvips/libvips/issues/1941 ?
+            #     return img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE, linear=True) \
+            #         .colourspace(img.interpretation)
 
             img = img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE)
         return img

commit 3d3fcb79523ebde8e2b47119428e47e785913e97

Fix regression introduced by 336b2681f8d6ff353053139746278f91bc1ebde4

pims/formats/utils/engines/vips.py
@@ -99,13 +99,12 @@ class VipsReader(AbstractReader):
     def vips_thumbnail(self, width, height, **loader_options):
         filename = self.vips_filename_with_options(str(self.format.path), **loader_options)
 
-        # Seems it has been fixed by https://github.com/libvips/libvips/pull/2120
-        # image = cached_vips_file(self.format)
-        # if image.interpretation in ("grey16", "rgb16"):
-        #     # Related to https://github.com/libvips/libvips/issues/1941 ?
-        #     return VIPSImage.thumbnail(filename, width, height=height,
-        #                                size=VIPSSize.FORCE, linear=True) \
-        #         .colourspace(image.interpretation)
+        image = cached_vips_file(self.format)
+        if image.interpretation in ("grey16", "rgb16"):
+            # Related to https://github.com/libvips/libvips/issues/1941 ?
+            return VIPSImage.thumbnail(filename, width, height=height,
+                                       size=VIPSSize.FORCE, linear=True) \
+                .colourspace(image.interpretation)
 
         return VIPSImage.thumbnail(filename, width, height=height, size=VIPSSize.FORCE)
 
@@ -133,11 +132,10 @@ class VipsHistogramReader(NullHistogramReader):
             return cached_vips_file(self.format)
 
         def _thumb(format):
-            # Seems it has been fixed by https://github.com/libvips/libvips/pull/2120
-            # image = cached_vips_file(format)
-            # if image.interpretation in ("grey16", "rgb16"):
-            #     return VIPSImage.thumbnail(str(format.path), 1024, linear=True)\
-            #         .colourspace(image.interpretation)
+            image = cached_vips_file(format)
+            if image.interpretation in ("grey16", "rgb16"):
+                return VIPSImage.thumbnail(str(format.path), 1024, linear=True)\
+                    .colourspace(image.interpretation)
             return VIPSImage.thumbnail(str(format.path), 1024)
 
         return self.format.get_cached('_vips_hist_image', _thumb, self.format)

pims/processing/operations.py
@@ -129,12 +129,6 @@ class ResizeImgOp(ImageOp):
 
     def _vips_impl(self, img, *args, **kwargs):
         if img.width != self.width or img.height != self.height:
-            # Seems it has been fixed by https://github.com/libvips/libvips/pull/2120
-            # if img.interpretation in ("grey16", "rgb16"):
-            #     # Related to https://github.com/libvips/libvips/issues/1941 ?
-            #     return img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE, linear=True) \
-            #         .colourspace(img.interpretation)
-
             img = img.thumbnail_image(self.width, height=self.height, size=VIPSSize.FORCE)
         return img
 