e74a8f17b88a1c3e9e120cd57b96725e9dc220a8
lrollus <loic.rollus@cytomine.com>
2021-11-30 09:12:56 +0100

    build wip

---
+++ scripts/ciCreateDependencyImage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+
+echo "************************************** Create dependency image ******************************************"
+
+file='./ci/version'
+VERSION_NUMBER=$(<"$file")
+
+echo "Launch Create dependency image for $VERSION_NUMBER"
+
+docker build --rm -f scripts/docker/Dockerfile-dependencies.build -t  cytomine/pims-dependencies:$VERSION_NUMBER .
