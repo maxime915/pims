1dbf5b6fa48521aecd26f84945d266c5935bc9d0
Chloé Marchal <chloe.marchal@cytomine.com>
2022-01-10 14:21:27 +0100

    Improved tests and added histogram representation testing

--- tests/test_tiff.py
+++ tests/test_tiff.py
@@ -10,50 +10,74 @@ from pims.files.file import (
     EXTRACTED_DIR, HISTOGRAM_STEM, ORIGINAL_STEM, PROCESSED_DIR, Path,
     SPATIAL_STEM, UPLOAD_DIR_PREFIX
 )
-from pims.utils.strings import unique_name_generator
+from pims.formats.utils.factories import FormatFactory
+from pims.api.utils.models import HistogramType
+from pims.processing.histograms.utils import build_histogram_file
 import pytest
 
-def get_image(path, image):
-	filepath = os.path.join(path, image)
-	# If image does not exist locally -> download image
-	if not os.path.exists(path):
-		os.mkdir("/data/pims/upload_test_tiff")
+def get_image(path, filename):
+    filepath = os.path.join(path, filename)
+    # If image does not exist locally -> download image
+    if not os.path.exists(path):
+        os.mkdir("/data/pims/upload_test_tiff")
 	
-	if not os.path.exists(filepath):
-		try:
-			url = f"https://data.cytomine.coop/open/uliege/{image}" #OAC
-			urllib.request.urlretrieve(url, filepath)
-		except Exception as e:
-			print("Could not download image")
-			print(e)
+    if not os.path.exists(filepath):
+        try:
+            url = f"https://data.cytomine.coop/open/uliege/{filename}" #OAC
+            urllib.request.urlretrieve(url, filepath)
+        except Exception as e:
+            print("Could not download image")
+            print(e)
 	
-	if not os.path.exists(path + "processed"):
-		try:
-			fi = FileImporter(f"/data/pims/upload_test_tiff/{image}")
-			fi.upload_dir = "/data/pims/upload_test_tiff"
-			fi.processed_dir = fi.upload_dir / Path("processed")
-			fi.mkdir(fi.processed_dir)
-		except Exception as e:
-			print(path + "/processed could not be created")
-			print(e)
+    if not os.path.exists(path + "processed"):
+        try:
+            fi = FileImporter(f"/data/pims/upload_test_tiff/{filename}")
+            fi.upload_dir = "/data/pims/upload_test_tiff"
+            fi.processed_dir = fi.upload_dir / Path("processed")
+            fi.mkdir(fi.processed_dir)
+        except Exception as e:
+            print(path + "/processed could not be created")
+            print(e)
 
-	if not os.path.exists(path+"/processed/visualisation.PYRTIFF"):
-		try:
-			fi.upload_path = Path(filepath)
-			original_filename = Path(f"{ORIGINAL_STEM}.PYRTIFF")
-			fi.original_path = fi.processed_dir / original_filename
-			fi.mksymlink(fi.original_path, fi.upload_path)
-			spatial_filename = Path(f"{SPATIAL_STEM}.PYRTIFF")
-			fi.spatial_path = fi.processed_dir / spatial_filename
-			fi.mksymlink(fi.spatial_path, fi.original_path)
-		except Exception as e:
-			print("Importation of images could not be done")
-			print(e)
+    if not os.path.exists(os.path.join(path,"processed/visualisation.PYRTIFF")):
+        if os.path.exists(os.path.join(path, "processed")):
+            fi = FileImporter(f"/data/pims/upload_test_tiff/{filename}")
+            fi.upload_dir = "/data/pims/upload_test_tiff"
+            fi.processed_dir = fi.upload_dir / Path("processed")
+        try:
+            fi.upload_path = Path(filepath)
+            original_filename = Path(f"{ORIGINAL_STEM}.PYRTIFF")
+            fi.original_path = fi.processed_dir / original_filename
+            fi.mksymlink(fi.original_path, fi.upload_path)
+            spatial_filename = Path(f"{SPATIAL_STEM}.PYRTIFF")
+            fi.spatial_path = fi.processed_dir / spatial_filename
+            fi.mksymlink(fi.spatial_path, fi.original_path)
+        except Exception as e:
+            print("Importation of images could not be done")
+            print(e)
+
+    if not os.path.exists(os.path.join(path, "processed/histogram")):
+        if os.path.exists(os.path.join(path, "processed")):
+            fi = FileImporter(f"/data/pims/upload_test_tiff/{filename}")
+            fi.upload_dir = Path("/data/pims/upload_test_tiff")
+            fi.processed_dir = fi.upload_dir / Path("processed")
+            original_filename = Path(f"{ORIGINAL_STEM}.PYRTIFF")
+            fi.original_path = fi.processed_dir / original_filename
+        try:
+            from pims.files.image import Image
+            fi.histogram_path = fi.processed_dir/Path(HISTOGRAM_STEM) #/data/pims/upload1641567540187798/processed/histogram
+            format = FormatFactory().match(fi.original_path)
+            fi.original = Image(fi.original_path, format=format)
+            fi.histogram = build_histogram_file(fi.original, fi.histogram_path, HistogramType.FAST)
+        except Exception as e:
+            print("Creation of histogram representation could not be done")
+            print(e)
 			
 def test_tiff_exists(image_path_tiff):
 	# Test if the file exists, either locally either with the OAC
-	get_image(image_path_tiff[0], image_path_tiff[1])
-	assert os.path.exists(os.path.join(image_path_tiff[0],image_path_tiff[1])) == True
+	path, filename = image_path_tiff
+	get_image(path, filename)
+	assert os.path.exists(os.path.join(path, filename)) == True
 
 def test_tiff_info(client, image_path_tiff):
 	response = client.get(f'/image/upload_test_tiff/{image_path_tiff[1]}/info')
@@ -65,38 +89,49 @@ def test_tiff_info(client, image_path_tiff):
 def test_tiff_metadata(client, image_path_tiff):
 	response = client.get(f'/image/upload_test_tiff/{image_path_tiff[1]}/metadata')
 	assert response.status_code == 200
-	assert response.json()['items'][7]["key"] == 'XResolution'
-	assert response.json()['items'][7]["value"] == '(4294967295, 69255)'
-	assert response.json()['items'][8]['key'] == 'YResolution'
-	assert response.json()['items'][8]["value"] == '(4294967295, 69255)'
-	assert response.json()['items'][10]['key'] == 'ResolutionUnit'
-	assert response.json()['items'][10]["value"] == "CENTIMETER"
+	lst = response.json()['items']
+	
+	index = next((index for (index, d) in enumerate(lst) if d["key"] == "XResolution"), None)
+	assert response.json()['items'][index]["value"] == '(4294967295, 69255)'
+	
+	index = next((index for (index, d) in enumerate(lst) if d["key"] == "YResolution"), None)
+	assert response.json()['items'][index]["value"] == '(4294967295, 69255)'
+	
+	index = next((index for (index, d) in enumerate(lst) if d["key"] == "ResolutionUnit"), None)
+	assert response.json()['items'][index]["value"] == "CENTIMETER"
 	
 def test_tiff_norm_tile(client, image_path_tiff):
-	response = client.get(f"/image/upload_test_tiff/{image_path_tiff[1]}/normalized-tile/zoom/3/ti/15", headers={"accept": "image/jpeg"})
-	assert response.status_code == 200
-	
-	img_response = Image.open(io.BytesIO(response.content))
-	width_resp, height_resp = img_response.size
-	assert width_resp == 256
-	assert height_resp == 256
+    _, filename = image_path_tiff
+    response = client.get(f"/image/upload_test_tiff/{filename}/normalized-tile/zoom/3/ti/15", headers={"accept": "image/png"})
+    assert response.status_code == 200
+
+    img_response = Image.open(io.BytesIO(response.content))
+    width_resp, height_resp = img_response.size
+
+    assert width_resp == 256
+    assert height_resp == 256
 	
 def test_tiff_thumb(client, image_path_tiff):
-	thumb_test(client, image_path_tiff[1], "tiff")
+	_, filename = image_path_tiff
+	thumb_test(client, filename, "tiff")
 	
 def test_tiff_resized(client, image_path_tiff):
-	resized_test(client, image_path_tiff[1], "tiff")
+	_, filename = image_path_tiff
+	resized_test(client, filename, "tiff")
 	
 def test_tiff_mask(client, image_path_tiff):
-	mask_test(client, image_path_tiff[1], "tiff")
+	_, filename = image_path_tiff
+	mask_test(client, filename, "tiff")
 	
 def test_tiff_crop(client, image_path_tiff):
-	crop_test(client, image_path_tiff[1], "tiff")
+	_, filename = image_path_tiff
+	crop_test(client, filename, "tiff")
 
-@pytest.mark.skip
+@pytest.mark.skip(reason="Does not return the correct response code")
 def test_tiff_crop_null_annot(client, image_path_tiff):
-	crop_null_annot_test(client, image_path_tiff[1], "tiff")
+	_, filename = image_path_tiff
+	crop_null_annot_test(client, filename, "tiff")
 
-@pytest.mark.skip
 def test_tiff_histogram_perimage(client, image_path_tiff):
-	histogram_perimage_test(client, image_path_tiff[1], "tiff")
+	_, filename = image_path_tiff
+	histogram_perimage_test(client, filename, "tiff")
