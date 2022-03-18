57a43a5ef045df884fcf52edbf0c889edd57608e
Ulysse Rubens <urubens@uliege.be>
2022-03-18 13:54:06 +0100

    Fix admissible physical size must be non-zero and positive for tiff

--- pims/formats/utils/engines/tifffile.py
+++ pims/formats/utils/engines/tifffile.py
@@ -181,7 +181,7 @@ class TifffileParser(AbstractParser):
             rational = (physical_size, 1)
         else:
             rational = physical_size
-        if rational[0] <= 0 and rational[1] <= 0:
+        if rational[0] <= 0 or rational[1] <= 0:
             return None
         return rational[1] / rational[0] * UNIT_REGISTRY(unit.name.lower())
 
